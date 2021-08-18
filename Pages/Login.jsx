faf3a9f05bfa907b59400b67b45eb6c15c6181de
ThatoMahloko <lordwaterphfffteat@gmail.com>
2021-08-18 09:04:17 +0200

    first commit

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { firebase } from '../Pages/firebase/config'
 
-const Login = ({ navigation }) =>
+const Login = ({ navigation }) =>{
 
     const [getEmail, setEmail] = useState('');
     const [getPassword, setPassword] = useState('');
